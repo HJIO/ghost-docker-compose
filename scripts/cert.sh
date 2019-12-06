0274f95c7139a022100151912ca6db295ac625ac
张智翔 <zhixiang@192-168-1-109.tpgi.com.au>
2019-12-06 18:21:53 +1100

    optimize performance

--- scripts/cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "checking certbot update..."
-docker pull certbot/certbot:latest
-docker pull certbot/dns-cloudflare:latest &&
-echo "creating certifications..."
-docker run -it --rm -v "/tmp/ssl:/etc/letsencrypt" certbot/certbot certonly -n -d $1
-echo "Certifications generated!"
